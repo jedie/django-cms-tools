8d10dabd7a2bfabe0dba5859846d063777e8f099
JensDiemer <git@jensdiemer.de>
2017-03-24 13:49:31 +0100

    i18n changes

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -14,13 +14,16 @@ from django_tools.unittest_utils.unittest_base import BaseTestCase
 
 @pytest.mark.django_db
 class AdminAnonymousTests(BaseTestCase):
+    """
+    Anonymous will be redirected to the login page.
+    """
+    def test_login_en(self):
+        response = self.client.get('/en/admin/', HTTP_ACCEPT_LANGUAGE='en')
+        self.assertRedirects(response, expected_url='http://testserver/en/admin/login/?next=/en/admin/')
 
-    def test_login(self):
-        """
-        Anonymous will be redirected to the login page.
-        """
-        response = self.client.get('/admin/')
-        self.assertRedirects(response, expected_url='http://testserver/admin/login/?next=/admin/')
+    def test_login_de(self):
+        response = self.client.get('/de/admin/', HTTP_ACCEPT_LANGUAGE='de')
+        self.assertRedirects(response, expected_url='http://testserver/de/admin/login/?next=/de/admin/')
 
 
 @pytest.mark.django_db
@@ -34,7 +37,7 @@ class AdminLoggedinTests(AdminAnonymousTests):
 
     def test_staff_admin_index(self):
         self.login(usertype='staff')
-        response = self.client.get('/admin/')
+        response = self.client.get('/en/admin/', HTTP_ACCEPT_LANGUAGE='en')
         self.assertResponse(response,
             must_contain=(
                 'Django administration',
@@ -48,7 +51,7 @@ class AdminLoggedinTests(AdminAnonymousTests):
 
     def test_superuser_admin_index(self):
         self.login(usertype='superuser')
-        response = self.client.get('/admin/')
+        response = self.client.get('/en/admin/', HTTP_ACCEPT_LANGUAGE='en')
         self.assertResponse(response,
             must_contain=(
                 'Django administration',
