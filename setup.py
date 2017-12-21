d91ec78d84848a08426ec3e51735ff23287c18d0
JensDiemer <git@jensdiemer.de>
2017-12-21 08:53:46 +0100

    update classifiers

--- setup.py
+++ setup.py
@@ -234,7 +234,12 @@ Programming Language :: Python
 Programming Language :: Python :: 3
 Programming Language :: Python :: 3.5
 Programming Language :: Python :: 3.6
+Programming Language :: Python :: 3 :: Only
+Programming Language :: Python :: Implementation :: CPython
+Programming Language :: Python :: Implementation :: PyPy
 Framework :: Django
+Framework :: Django :: 1.11
+Framework :: Django :: 1.8
 Topic :: Internet
 """
 
