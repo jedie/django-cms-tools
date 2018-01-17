75638cea6995de1251f96dd43df30ac704c966dd
JensDiemer <git@jensdiemer.de>
2018-01-17 15:48:19 +0100

    move test project files and ignore them

--- django_cms_tools_test_project/settings.py
+++ django_cms_tools_test_project/settings.py
@@ -26,7 +26,7 @@ from django.utils.translation import ugettext_lazy as _
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -127,7 +127,7 @@ TEMPLATES = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'test_project_db.sqlite3'),
         # 'NAME': ":memory:"
     }
 }
